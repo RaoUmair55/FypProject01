00fdad88df80dbba2d39f294b944e8a084329b26
Usman Ejaz <mohd.usman2000@gmail.com>
2025-05-04 17:39:46 +0500

    fixed logout bug

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -6,17 +6,17 @@ export const protectRoute = async (req, res, next) => {
     try {
         const token = req.cookies.token;
         if (!token) {
-            return res.status(401).json({ message: "Unauthorized" });
+            return res.status(401).json({ error: "Unauthorized" });
         }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await User.findById(decoded.userId).select("-password");
         if (!user) {
-            return res.status(401).json({ message: "Unauthorized" });
+            return res.status(401).json({ error: "Unauthorized" });
         }
         req.user = user;
         next();
     } catch (error) {
         console.error("Error in protectRoute:", error);
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500).json({ error: "Internal server error" });
     }
 }
