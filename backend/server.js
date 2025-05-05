55a4c31e14d6c33492b8e09d6822a9bfab0d7f3e
Usman Ejaz <mohd.usman2000@gmail.com>
2025-05-05 20:14:45 +0500

    fixed minor bug

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({limit:"5mb"}));   // limit shouldn't be to large as it makes it more susceptible to DOS attack
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
