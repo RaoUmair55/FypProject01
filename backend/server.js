1e8f9c44b7003a396d7af0b4e01f58d4f3f9fa61
RaoUmair55 <raoumair554@gmail.com>
2025-06-02 22:14:18 +0500

    Refactor CORS configuration in server.js and comment out API proxy settings in vite.config.js

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+
+app.use(cors({
+  origin:"https://campusbuzzz.com",
+  credentials: true, // Allow cookies to be sent with requests
+}));
+
 app.use(express.json({limit:"5mb"}));   // limit shouldn't be to large as it makes it more susceptible to DOS attack
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public")) //pdf picture etc for public anyone can accept
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,10 +11,10 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true
-      }
+      // "/api": {
+      //   target: "http://localhost:8000",
+      //   changeOrigin: true
+      // }
     }
   }
 })
